8e498f26f213f5e26b77a4b39984de61a6419ba0
ahmed <ahmedelkazaz@gmail.com>
2024-10-10 12:03:36 +0300

    add card theme in theme data

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/presentation/color_manager.dart';
+import 'package:e_commerce_app/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
@@ -10,6 +11,11 @@ ThemeData getApplicationTheme() {
     disabledColor: ColorManager.grey1,
 
 //? card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey1,
+      elevation: AppSize.s4,
+    ),
 //! text theme
 
 // app bar theme
--- lib/presentation/values_manager.dart
+++ lib/presentation/values_manager.dart
@@ -37,6 +37,9 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s6 = 6.0;
   static const double s10 = 10.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;
@@ -52,5 +55,4 @@ class AppSize {
   static const double s34 = 34.0;
   static const double s36 = 36.0;
   static const double s38 = 38.0;
-  static const double s8 = 8.0;
 }
