4bb05fcec88d72093bab9ed5c97a5042a0b62a11
ahmed <ahmedelkazaz@gmail.com>
2024-10-10 11:40:48 +0300

    add styles manager and values manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,40 @@
+import 'package:e_commerce_app/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: color,
+    fontFamily: FontConstants.fontFamily,
+  );
+}
+
+//regular style
+TextStyle getRegularStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.regular, color);
+}
+
+//semiBold style
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.semiBold, color);
+}
+
+//medium style
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+//regular style
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.light, color);
+}
+
+//bold style
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.bold, color);
+}
