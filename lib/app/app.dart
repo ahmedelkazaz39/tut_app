5ab47a6959b4bc90f0fb6cc8d2a26b597fd55e81
ahmed <ahmedelkazaz@gmail.com>
2024-10-10 10:24:42 +0300

    create structure of project and color manager and assets folder

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  //! named constructor for create singleton pattern
+  MyApp._internal();
+  static final MyApp _instance = MyApp._internal(); //? singleton instance
+  factory MyApp() => _instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = const Color(0xffED9728);
+  static Color darkGray = const Color(0xff525252);
+  static Color lightGrayOfBorder = const Color(0xff304654);
+  static Color lightGray = const Color(0xff9E9E9E);
+}
