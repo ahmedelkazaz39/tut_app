29f3c8253b64c185c3d30b3897b5926a90c49889
ahmed <ahmedelkazaz@gmail.com>
2024-10-10 18:17:59 +0300

    create store detailes  view

---
+++ lib/presentation/store_detailes/store_detailes_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoreDetailesView extends StatefulWidget {
+  const StoreDetailesView({super.key});
+
+  @override
+  State<StoreDetailesView> createState() => _StoreDetailesViewState();
+}
+
+class _StoreDetailesViewState extends State<StoreDetailesView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
